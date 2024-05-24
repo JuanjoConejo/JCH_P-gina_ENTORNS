34cd20ade1de4cc285f96a02e37f5ec1508eeb59
manolo <manolo@gmail.com>
2024-05-24 09:35:32 +0200

    Millora

--- src/curriculum/index.js
+++ src/curriculum/index.js
@@ -8,6 +8,15 @@ const imagenURL = "https://store-images.s-microsoft.com/image/apps.31120.9007199
 // URL del perfil de LinkedIn de Juanjo Conejo
 const linkedinURL = "https://www.linkedin.com/in/juanjo-conejo-9150462b6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
 
+// URL del perfil de Twitter de Juanjo Conejo
+const twitterURL = "https://twitter.com/JuanjoConejo?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor";
+
+// URL del perfil de Instagram de Juanjo Conejo
+const instagramURL = "https://www.instagram.com/juanjoconejo/?hl=es";
+
+// URL del perfil de GitHub de Juanjo Conejo
+const githubURL = "https://github.com/JuanjoConejo";
+
 // Contenido del currículum
 const curriculumJuanjo = "¡Hola! Soy Juanjo Conejo Hernández, un entusiasta desarrollador web apasionado por crear experiencias digitales increíbles. Con experiencia en tecnologías como JavaScript, React y CSS, estoy siempre buscando aprender y mejorar mis habilidades. ¡Echa un vistazo a mi currículum a continuación!";
 
@@ -16,8 +25,23 @@ function Curriculum() {
         <div className="curriculum-container">
             <p className="descripcion">{curriculumJuanjo}</p>
             {/* Enlace clickeable que lleva al perfil de LinkedIn */}
-            <a href={linkedinURL} target="_blank" rel="noopener noreferrer">
-                <img src={imagenURL} alt="Imagen de ejemplo" className="curriculum-img-small" />
+            <a href={linkedinURL} target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                <img src={imagenURL} alt="LinkedIn" className="curriculum-img-small" />
+            </a>
+            {/* Fin del enlace */}
+            {/* Enlace clickeable que lleva al perfil de Twitter */}
+            <a href={twitterURL} target="_blank" rel="noopener noreferrer" className="twitter-link">
+                <img src="https://turbologo.com/articles/wp-content/uploads/2019/07/twitter-bird-logo.png.webp" alt="Twitter" className="curriculum-img-small" />
+            </a>
+            {/* Fin del enlace */}
+            {/* Enlace clickeable que lleva al perfil de Instagram */}
+            <a href={instagramURL} target="_blank" rel="noopener noreferrer" className="instagram-link">
+                <img src="https://i.blogs.es/c04a74/instagram/840_560.jpg" alt="Instagram" className="curriculum-img-small" />
+            </a>
+            {/* Fin del enlace */}
+            {/* Enlace clickeable que lleva al perfil de GitHub */}
+            <a href={githubURL} target="_blank" rel="noopener noreferrer" className="github-link">
+                <img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" alt="GitHub" className="curriculum-img-small" />
             </a>
             {/* Fin del enlace */}
             <br />
--- src/gustos/agrada.js
+++ src/gustos/agrada.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import './agrada.css'; // Importa el archivo CSS de quem'agrada
+import './agrada.css'; // Importa el archivo CSS de estilos
 
-// Array de cosas que et agraden amb títols i descripcions
+// Array de cosas que te gustan con títulos y descripciones
 const cosesQueEtAgraden = [
-  { titulo: "Valorant", descripcion: "M'agrada jugar a Valorant en els meus temps lliures.", imagen: "https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-d9ca2c0fbaff9d80e8dedfbd726aa438" },
-  { titulo: "Futbol", descripcion: "Gaudeixo veient i jugant a futbol.", imagen: "https://e00-mx-marca.uecdn.es/mx/assets/multimedia/imagenes/2023/12/11/17023144752291.jpg" },
-  { titulo: "Sortir de festa", descripcion: "M'encanta socialitzar i divertir-me en festes.", imagen: "https://www.economistjurist.es/wp-content/uploads/sites/2/2023/07/la1.jpg" }
-  // Pots afegir més coses que et agradin aquí
+  {
+    titulo: "Valorant",
+    descripcion: "M'agrada jugar a Valorant en els meus temps lliures.",
+    imagen: "https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-d9ca2c0fbaff9d80e8dedfbd726aa438"
+  },
+  {
+    titulo: "Futbol",
+    descripcion: "Gaudeixo veient i jugant a futbol.",
+    imagen: "https://e00-mx-marca.uecdn.es/mx/assets/multimedia/imagenes/2023/12/11/17023144752291.jpg"
+  },
+  {
+    titulo: "Sortir de festa",
+    descripcion: "M'encanta socialitzar i divertir-me en festes.",
+    imagen: "https://www.economistjurist.es/wp-content/uploads/sites/2/2023/07/la1.jpg"
+  },
+  {
+    titulo: "Bàsquet",
+    descripcion: "Disfruto jugant a bàsquet amb els meus amics.",
+    imagen: "https://www.nbamaniacs.com/wp-content/uploads/2017/08/GettyImages-245168-768x527.jpg"
+  },
+  {
+    titulo: "Fortnite",
+    descripcion: "M'agrada jugar a Fortnite i competir amb altres jugadors.",
+    imagen: "https://cdn1.epicgames.com/offer/fn/Blade_2560x1440_2560x1440-95718a8046a942675a0bc4d27560e2bb"
+  },
+  {
+    titulo: "Disseny de videojocs 3D",
+    descripcion: "M'apassiona dissenyar i crear videojocs en 3D.",
+    imagen: "https://valenciaplaza.com/public/Image/2015/12/3d_NoticiaAmpliada.jpg"
+  }
 ];
 
 function Gust() {
@@ -16,7 +42,7 @@ function Gust() {
       <ul className="me-gusta-list">
         {cosesQueEtAgraden.map((gust, index) => (
           <li key={index} className="me-gusta-item">
-            <span className="me-gusta-titulo">{gust.titulo}:</span> 
+            <span className="me-gusta-titulo">{gust.titulo}:</span>
             <span className="me-gusta-descripcion">{gust.descripcion}</span>
             {gust.imagen && <img src={gust.imagen} alt={gust.titulo} className="me-gusta-imagen" />}
           </li>
